77b15ed5c12dcf68ed5386bf136f1173894d657f
Dillon Smart <dillon.smart@ouritservices.co.uk>
2022-01-27 14:21:26 +0000

    Some methods added

--- src/Arr.php
+++ src/Arr.php
@@ -16,4 +16,31 @@ class Arr {
         return array_push($arr, $value);
     }
 
+    /**
+     * Splice the given array.
+     * By default, the first index of the array is spliced.
+     *
+     * @param $arr
+     * @param $offset Defaults is 0
+     * @param $length Default is 1
+     * @return array
+     */
+    public static function splice($arr, $offset = 0, $length = 1)
+    {
+        return array_splice($array, $offset, $length);
+    }
+
+    /**
+     * Splice the given array from the end.
+     * By default, the last index will be remvoed.
+     *
+     * @param $arr
+     * @param $length
+     * @return mixed
+     */
+    public static function splice_end($arr, $length = 1)
+    {
+        return array_splace($arr, sizeof($arr) - $length, $length);
+    }
+
 }
